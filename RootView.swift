109f78564e451399a4d464fcfca848f302231c9b
petersudario <pedrohesudario@hotmail.com>
2025-04-21 02:19:27 -0300

    Code refactoring

---
+++ RootView.swift
@@ -0,0 +1,59 @@
+//
+//  RootView.swift
+//  Meet Pepo
+//
+//  Created by Pedro Henrique Sudario da Silva on 21/04/25.
+//
+
+import SwiftUI
+import SpriteKit
+
+struct RootView: View {
+    @State private var showSplash = true
+    @State private var scene = PhysicsScene(size: UIScreen.main.bounds.size)
+    @State private var started = false
+
+    var body: some View {
+        ZStack {
+            Rectangle().fill(Color.black)
+
+            ContentView()
+
+            
+            if showSplash {
+                Rectangle().fill(Color.black)
+            }
+            
+
+            SpriteView(
+                scene: scene,
+                options: [.allowsTransparency]
+            )
+            .ignoresSafeArea()
+            .allowsHitTesting(false)
+            
+
+            if showSplash {
+                SplashOverlay(startAction: {
+                    if started == false {
+                        scene.startFalling()
+                        started = true
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                            withAnimation { showSplash = false }
+                        }
+                    }
+
+                })
+                .ignoresSafeArea()
+            }
+        }
+        .onChange(of: showSplash) { newValue in
+            if !newValue {
+                scene.backgroundColor = .clear
+                scene.popAllSprites()
+            }
+        }
+    }
+}
+
+
